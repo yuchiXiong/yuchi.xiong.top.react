8067f33bacfc807e4aefb62ab5bfdaf19078188d
yuchi <yuchi.xiong@foxmail.com>
2020-08-04 00:11:33 +0800

    配置了antd自定义主题

--- src/index.js
+++ src/index.js
@@ -5,15 +5,12 @@ import { Button } from 'antd';
 import * as serviceWorker from './serviceWorker';
 import App from './App';
 
-// import './index.scss';
-// import './assets/styles/antd.less';
-import './index.less';
-
+import './index.scss';
+import './assets/styles/antd.less';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Button>1111</Button>
-    {/* <App /> */}
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
